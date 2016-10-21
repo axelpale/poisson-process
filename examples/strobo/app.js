f10eff3436c951fea5faf1adea9ccd125fca77d8
Akseli Palen <akseli.palen@gmail.com>
2016-10-22 00:33:45 +0300

    Change example's interval to 500

--- examples/strobo/app.js
+++ examples/strobo/app.js
@@ -2,7 +2,7 @@
 var bg = document.body;
 var bgState = true;
 
-var p = PoissonProcess.create(80, function () {
+var p = PoissonProcess.create(500, function () {
   console.log('tick');
   if (bgState) {
     bg.style.backgroundColor = 'black';
